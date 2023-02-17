d8235e65f7c752160fdef82d3ab8ab62aa7eceec
wei11223344 <119714500+wei11223344@users.noreply.github.com>
2023-02-17 18:21:49 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,3 +1,22 @@
-Test = function() {
-alert("hi");
+export function hacktowers(setowers) {
+    (async () => { /* This is to prevent "identifier i is already declared errors" */
+    let i = document.createElement('iframe');
+    document.body.append(i);
+    window.confirm = i.contentWindow.confirm.bind(window);
+    i.remove();
+    Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']]]).cache).find(x => x.exports?.a?.get).exports.a.get("https://" + (location.host.startsWith("dashboard") ? location.host : "play.blooket.com") + "/api/games?gameId=6368436a976422d8a3f70cd7").then(x => parseInt(`0${x.data.questions.find(x => x.question == "../cheats/tower-defense-2/maxTowers.js")?.answers?.[0]}`)).then(async x => {
+        if (1676511529174 > x || confirm("This cheat is outdated and might be bugged, would you still like to run it? You can find regularly updated cheats here https://github.com/Minesraft2/Blooket-Cheats")) {
+            /* Update Checker end */
+            Object.values(document.querySelector('#app > div > div'))[1].children[0]._owner.stateNode.state.towers.forEach(tower => {
+                tower.stats.dmg = hacktowers;
+                tower.stats.fireRate = hacktowers;
+                tower.stats.ghostDetect = true;
+                tower.stats.maxTargets = hacktowers;
+                tower.stats.numProjectiles &&= hacktowers;
+                tower.stats.range = hacktowers;
+                if (tower.stats.auraBuffs) for (const buff in tower.stats.auraBuffs) tower.stats.auraBuffs[buff] *= 100;
+            });
+        }
+    });
+})();
 }
