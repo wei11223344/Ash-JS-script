c4265a8b399f3072c3e8d1052ba8a6dac7bc78f3
wei11223344 <119714500+wei11223344@users.noreply.github.com>
2023-02-17 20:02:44 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,9 +1,9 @@
 // newdiv.js
-export default function newdiv(color, sizex, sizey, name) {
+module.exports = function newdiv(color, sizex, sizey, name) {
   const a = document.createElement("div");
   a.style.backgroundColor = color;
   a.style.width = sizex;
   a.style.height = sizey;
   a.id = name;
   return a;
-}
+};
