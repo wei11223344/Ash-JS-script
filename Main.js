a9433c711ed72ba78a8224dd2359517ded0fa457
wei11223344 <119714500+wei11223344@users.noreply.github.com>
2023-02-17 20:10:52 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,9 +1,10 @@
-// newdiv.js
 function newdiv(color, sizex, sizey, name) {
-  const a = document.createElement("div");
-  a.style.backgroundColor = color;
-  a.style.width = sizex;
-  a.style.height = sizey;
-  a.id = name;
-  return a;
+    a = document.createElement("div");
+    a.style.backgroundColor = color;
+    a.style.width = sizex;
+    a.style.height = sizey;
+    a.id = name;
+    return a;
 }
+
+module.exports = {newdiv};
