465e260b8cd0a0c8ea4639d95bcb790e07438867
wei11223344 <119714500+wei11223344@users.noreply.github.com>
2023-02-17 19:27:55 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,1 +1,8 @@
-alert("test")
+function newdiv(color, sizex, sizey, name) {
+    a = document.createElement("div");
+    a.style.backgroundColor = color;
+    a.style.width = sizex;
+    a.style.height = sizey;
+    a.id = name;
+    return a;
+}
